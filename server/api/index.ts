e468409e28f54e0903781cc5e4c2ca478d9d991a
AlexNg <contact@ngjx.org>
2024-03-22 01:39:10 +0800

    feat: Add /api route

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,7 +1,8 @@
 import express from 'express'
 const app = express()
 
-app.get('/', (_, res) => res.send('Hello World!'))
+app.get('/', (_, res) => res.status(200).send('Hello World!'))
+app.get('/api', (_, res) => res.status(200).send('This is an API!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
