6a35951e11dfdb77836019e4f5e540e3d4e93c83
AlexNg <contact@ngjx.org>
2024-03-22 13:33:47 +0800

    feat: Layout setup

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import * as React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import '@styles/globals.css'
+
+
+export const Layout = ({ children }): JSX.Element => {
+  return (
+    <main className='flex min-h-screen min-w-full max-w-full flex-col'>
+      {/* Navbar here */}
+      {children}
+      {/* Footer here */}
+    </main>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Layout>
+      <App />
+    </Layout>
   </React.StrictMode>,
 )
