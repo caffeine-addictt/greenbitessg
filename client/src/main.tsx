e27e526013d244a4b9e60841ffd8da70c3015379
AlexNg <contact@ngjx.org>
2024-03-23 02:48:25 +0800

    refactor: remove children prop

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,8 +2,8 @@ import * as React from 'react'
 import ReactDOM from 'react-dom/client'
 import '@styles/globals.css'
 
+export const Layout = (): JSX.Element => {
 
-export const Layout = ({ children }: { children: React.ReactNode }): JSX.Element => {
   return (
     <main className='flex min-h-screen min-w-full max-w-full flex-col'>
       {/* Navbar here */}
@@ -15,7 +15,6 @@ export const Layout = ({ children }: { children: React.ReactNode }): JSX.Element
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Layout>
-    </Layout>
+      <Layout />
   </React.StrictMode>,
 )
