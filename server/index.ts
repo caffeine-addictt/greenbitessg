b27d4cfcf4a924ac9a806bfe79ebbfa789233075
AlexNg <contact@ngjx.org>
2024-03-28 21:30:07 +0800

    feat: Allow cors

--- server/index.ts
+++ server/index.ts
@@ -15,11 +15,13 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import cors from 'cors';
 import path from 'path';
 import express from 'express';
 const app = express();
 
 // Server Config
+app.use(cors({ origin: '*', credentials: true }));
 app.use(express.json());
 
 // Register routes
