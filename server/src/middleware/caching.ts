6252605b6dfce6098823034942c65bdee7adf041
AlexNg <contact@ngjx.org>
2024-03-29 20:11:48 +0800

    feat(caching): Conforming to RFC 7231
    This will make caching only consider GET and HEAD requests, with the option to `ignoreRFC` to cache on PATCH and POST requests as well.
    The next commit will expose `prefix` in route caching to enable caching in respect to unique identifiers like userIDs.
    
    RFC Ref: https://www.rfc-editor.org/rfc/rfc7231#section-4.3

--- server/src/middleware/caching.ts
+++ server/src/middleware/caching.ts
@@ -18,8 +18,23 @@
 import express from 'express';
 import memCache from 'memory-cache';
 
+import Methods from '../utils/http-methods';
+
 // Config
 const cacheTime = 10000 as const; // 10seconds
+
+/**
+ * Conform to RFC 7231
+ * @see {@link https://www.rfc-editor.org/rfc/rfc7231#section-4.3}
+ */
+export const CachableCode: number[] = [
+  200, 203, 204, 206, 300, 301, 404, 405, 410, 414, 501,
+] as const;
+export const CachableMethod: Array<keyof typeof Methods> = [
+  'GET',
+  'HEAD',
+  'OPTIONS',
+] as const;
 export interface CachedResponse<B> {
   contentType?: string;
   body?: B;
@@ -32,25 +47,37 @@ const iCachingMiddleware = (
   next: express.NextFunction,
   prefix: string = '__global__',
   time?: number,
+  ignoreRFC: boolean = false,
 ) => {
+  const cachableMethod = CachableMethod.includes(
+    req.method.toUpperCase() as keyof typeof Methods,
+  );
   const key = `${prefix}${req.originalUrl || req.url}${req.method}`;
-  const rawCached: string = memCache.get(key);
 
-  if (rawCached) {
-    const cached: CachedResponse<unknown> = JSON.parse(rawCached);
+  if (!ignoreRFC || cachableMethod) {
+    const rawCached: string = memCache.get(key);
 
-    if (cached.contentType) res.set('Content-Type', cached.contentType);
-    return res.status(302).send(cached.body);
+    if (rawCached) {
+      const cached: CachedResponse<unknown> = JSON.parse(rawCached);
+
+      if (cached.contentType) res.set('Content-Type', cached.contentType);
+      return res.status(302).send(cached.body);
+    }
   }
 
   // @ts-expect-error Alter Response object without having to update other references
   res.sendResponse = res.send;
   res.send = (body) => {
-    const data: string = JSON.stringify({
-      contentType: res.get('Content-Type'),
-      body: body,
-    } satisfies CachedResponse<typeof body>);
-    memCache.put(key, data, time || cacheTime);
+    if (
+      !ignoreRFC ||
+      (cachableMethod && CachableCode.includes(res.statusCode))
+    ) {
+      const data: string = JSON.stringify({
+        contentType: res.get('Content-Type'),
+        body: body,
+      } satisfies CachedResponse<typeof body>);
+      memCache.put(key, data, time || cacheTime);
+    }
 
     // @ts-expect-error Alter Response object without having to update other references
     return res.sendResponse(body);
@@ -60,9 +87,9 @@ const iCachingMiddleware = (
 };
 
 export const routeCachingMiddleware =
-  (time?: number) =>
+  (time?: number, ignoreRFC?: boolean) =>
   (req: express.Request, res: express.Response, next: express.NextFunction) =>
-    iCachingMiddleware(req, res, next, '__route__', time);
+    iCachingMiddleware(req, res, next, '__route__', time, ignoreRFC);
 
 const cachingMiddleware = (
   req: express.Request,
