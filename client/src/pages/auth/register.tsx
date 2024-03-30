43d3a2d2a07c93029ef46f6ff7b0a35d95d73f36
AlexNg <contact@ngjx.org>
2024-03-31 00:44:32 +0800

    feat: zod Schema for registering form

--- client/src/pages/auth/register.tsx
+++ client/src/pages/auth/register.tsx
@@ -14,3 +14,77 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import * as React from 'react';
+
+import * as z from 'zod';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { Form } from '@components/ui/form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+// Setup
+const registerFormSchema = z
+  .object({
+    username: z
+      .string({
+        invalid_type_error: 'Please provide a username!',
+        required_error: 'Please provide a username!',
+      })
+      .min(1, { message: 'Please provide a username!' })
+      .min(3, { message: 'Username needs to be at least 3 characters!' })
+      .max(20, { message: 'Username cannot be longer than 20 characters!' })
+      .regex(/[\w\d-_]/, {
+        message: 'Username may only contain alphanumeric characters and (-_)',
+      }),
+
+    email: z
+      .string({
+        invalid_type_error: 'Please provide an email!',
+        required_error: 'Please provide an email!',
+      })
+      .min(1, { message: 'Please provide an email!' })
+      .email({ message: 'Email is not valid!' }),
+
+    password: z
+      .string({
+        invalid_type_error: 'Please provide a password!',
+        required_error: 'Please provide a password!',
+      })
+      .min(1, { message: 'Please provide a password!' })
+      .min(8, { message: 'Password needs to be at least 8 characters!' })
+      .regex(/[a-z]/, {
+        message:
+          'Password needs to contain at least 1 lower case character! (a-z)',
+      })
+      .regex(/[A-Z]/, {
+        message:
+          'Password needs to contain at least 1 upper case character! (A-Z)',
+      })
+      .regex(/[\d]/, {
+        message: 'Password needs to contain at least 1 digit! (0-9)',
+      })
+      .regex(/[!#$%&?'"]/, {
+        message:
+          'Password needs to contain at least 1 special character! (!#$%&?\'")',
+      })
+      .regex(/^[a-zA-Z\d]+$/, {
+        message:
+          'Passwords may only contain alphanumeric characters and (!#$%&?\'")',
+      }),
+
+    confirm: z
+      .string({
+        invalid_type_error: 'Please retype your password!',
+        required_error: 'Please retype your password!',
+      })
+      .min(1, { message: 'Please retype your password!' }),
+
+    agreeMarketting: z.boolean().optional(),
+
+    agreePolicy: z.boolean(),
+  })
+  .refine((data) => data.password === data.confirm, {
+    message: 'Passwords do not match!',
+    path: ['confirm'],
+  });
+
