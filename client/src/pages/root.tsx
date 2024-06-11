23db308aff78e8af97e7a98b19772a513d580f69
Jy <tanjy0206@gmail.com>
2024-06-11 15:02:47 +0800

    feat: updated start now section

--- client/src/pages/root.tsx
+++ client/src/pages/root.tsx
@@ -5,48 +5,67 @@
  */
 
 import type { PageComponent } from '@pages/route-map';
+import { Button } from '@components/ui/button';
 
 const RootPage: PageComponent = (props) => {
-  return <div {...props} className="flex-col items-center">
+  return (
+    <div {...props} className="flex-col items-center">
+      <div className="container">
+        <div className=""></div>
+      </div>
 
-    <div className="container">
-      <h1 className="text-2xl font-bold text-center">About Us</h1>
-      <div className="flex flex-col md:flex-row items-center">
-        <img className="w-full md:w-1/2" alt=""></img>
-        <div className='mt-4 md:mt-0 md:ml-0'>
-          <h3 className='text-2xl'> What is GreenBitesSG?</h3>
-          <p>Here at GreenBitesSG we are a nation wide movement that focuses on fighting food wastage, to advance
-            Singapore's national agenda on sustainable development by giving you a platform to learn and
-            practice sustainable food habits.
-          </p>
+      <div className="container">
+        <h1 className="text-center text-2xl font-bold">About Us</h1>
+        <div className="flex flex-col items-center md:flex-row">
+          <img className="w-full md:w-1/2" alt=""></img>
+          <div className="mt-4 md:ml-0 md:mt-0">
+            <h3 className="text-2xl"> What is GreenBitesSG?</h3>
+            <p>
+              Here at GreenBitesSG we are a nation wide movement that focuses on
+              fighting food wastage, to advance Singapore's national agenda on
+              sustainable development by giving you a platform to learn and
+              practice sustainable food habits.
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center md:flex-row-reverse">
+          <img className="w-full md:w-1/2" alt=""></img>
+          <div className="mt-4 md:ml-0 md:mt-0">
+            <h3 className="text-2xl"> Why is GreenBitesSG important?</h3>
+            <p>
+              Food Wastage is a global challenge, and we have to take firm
+              actions to do our part for Singapore to reduce food wastage.
+            </p>
+          </div>
         </div>
       </div>
-      <div className="flex flex-col md:flex-row-reverse items-center">
-        <img className="w-full md:w-1/2" alt=""></img>
-        <div className='mt-4 md:mt-0 md:ml-0'>
-          <h3 className='text-2xl'> Why is GreenBitesSG important?</h3>
-          <p>Food Wastage is a global challenge, and we have to take firm actions to do our part
-             for Singapore to reduce food wastage.
-          </p>
+
+      <div className="container">
+        <h1 className="text-center text-2xl font-bold">Our Partners</h1>
+        <div className="flex flex-col items-center md:flex-row">
+          <img className="w-full md:w-1/2" alt=""></img>
+          <img className="w-full md:w-1/2" alt=""></img>
+        </div>
+      </div>
+
+      <div className="container">
+        <h1 className="text-center text-2xl font-bold">Start Now</h1>
+        <div className="flex flex-col items-center md:flex-row">
+          <img className="w-full md:w-1/2" alt=""></img>
+          <div className="mt-4 md:ml-0 md:mt-0">
+            <p>
+              What are you waiting for? Create an account now and join the
+              community in combatting food wastage!
+            </p>
+            <a href="/register">
+              <Button variant="secondary" size="default">
+                Start Now
+              </Button>
+            </a>
+          </div>
         </div>
       </div>
     </div>
-
-    <div className="container">
-      <h1 className="text-2xl font-bold text-center">Our Partners</h1>
-    </div>
-
-    <div className="container">
-      <h1 className="text-2xl font-bold text-center">Start Now</h1>
-      <div className="flex flex-col md:flex-row items-center">
-        <img className="w-full md:w-1/2" alt=""></img>
-        <div className='mt-4 md:mt-0 md:ml-0'>
-          <h3 className='text-2xl'></h3>
-          <p>
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>;
+  );
 };
 export default RootPage;
