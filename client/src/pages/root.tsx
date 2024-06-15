07c33a9a83c6e28bf788184b0fea5548a63b348d
KenNoYu <90316680+KenNoYu@users.noreply.github.com>
2024-06-15 16:14:24 +0800

    Feat: Client side homepage basic layout (#159)
    * feat: basic homepage layout
    
    * feat: updated start now section
    
    * feat: Increased spacing  between each section
    
    * fix: centered items on x axis
    
    ---
    
    Reviewed-by: AlexNg <contact@ngjx.org>
    Signed-off-by: Jy <tanjy0206@gmail.com>

--- client/src/pages/root.tsx
+++ client/src/pages/root.tsx
@@ -5,8 +5,71 @@
  */
 
 import type { PageComponent } from '@pages/route-map';
+import { Button } from '@components/ui/button';
 
 const RootPage: PageComponent = (props) => {
-  return <div {...props}>hi</div>;
+  return (
+    <div {...props} className="flex-col items-center">
+      <div className="container">
+        <div className=""></div>
+      </div>
+
+      <div className="container mx-auto mt-16 flex-col">
+        <h1 className="text-center text-2xl font-bold ">About Us</h1>
+        <div className="mt-8 flex flex-col items-center md:flex-row">
+          <img className="w-full md:w-1/2" alt=""></img>
+          <div className="mt-4 w-full md:ml-0 md:mt-0 md:w-1/2">
+            <h3 className="text-center text-2xl"> What is GreenBitesSG?</h3>
+            <p className="mt-3">
+              Here at GreenBitesSG we are a nation wide movement that focuses on
+              fighting food wastage, to advance Singapore's national agenda on
+              sustainable development by giving you a platform to learn and
+              practice sustainable food habits.
+            </p>
+          </div>
+        </div>
+        <div className="mt-8 flex flex-col items-center md:flex-row-reverse">
+          <img className="w-full md:w-1/2" alt=""></img>
+          <div className="mt-4 w-full md:ml-0 md:mt-0 md:w-1/2">
+            <h3 className="text-center text-2xl">
+              {' '}
+              Why is GreenBitesSG important?
+            </h3>
+            <p className="mt-7">
+              Food Wastage is a global challenge and every second counts in this
+              problem, we have to make every one of our actions count, and take
+              firm actions to do our part for Singapore to reduce food wastage.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="container mx-auto mt-20">
+        <h1 className="text-center text-2xl font-bold">Our Partners</h1>
+        <div className="flex flex-col items-center md:flex-row">
+          <img className="w-full md:w-1/2" alt=""></img>
+          <img className="w-full md:w-1/2" alt=""></img>
+        </div>
+      </div>
+
+      <div className="container mx-auto mt-20">
+        <h1 className="text-center text-2xl font-bold">Start Now</h1>
+        <div className="mt-8 flex flex-col items-center md:flex-row">
+          <img className="w-full md:w-1/2" alt=""></img>
+          <div className="mt-4 w-full md:ml-0 md:mt-0 md:w-1/2">
+            <p className="mb-4">
+              So what are you waiting for? Food wastage won't wait for you so
+              join us and the community in combatting food wastage!
+            </p>
+            <a href="/register" className="flex justify-center">
+              <Button variant="secondary" size="default">
+                Start Now
+              </Button>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default RootPage;
