4dbc6b78a909720847e1978210713ec92eac0586
Jun Xiang (カエデ) <contact@ngjx.org>
2024-06-16 16:28:08 +0800

    Feature/database setup (#161)
    * chore(deps): Add drizzle dependencies
    
    Signed-off-by: AlexNg <contact@ngjx.org>
    
    * chore(deps): bump braces from 3.0.2 to 3.0.3
    
    This fixes security vulnerability warning
    
    Signed-off-by: AlexNg <contact@ngjx.org>
    
    * chore(deps): Add PostgreSQL db
    
    Signed-off-by: AlexNg <contact@ngjx.org>
    
    * feat: Load environment
    
    Signed-off-by: AlexNg <contact@ngjx.org>
    
    * feat: Initialize database connection
    
    Signed-off-by: AlexNg <contact@ngjx.org>
    
    * feat: Drizzle config
    
    Signed-off-by: AlexNg <contact@ngjx.org>
    
    * chore: Add db comamnds
    
    Signed-off-by: AlexNg <contact@ngjx.org>
    
    * fix: Use ESNext
    
    Signed-off-by: AlexNg <contact@ngjx.org>
    
    * feat: Basic user schema
    
    Signed-off-by: AlexNg <contact@ngjx.org>
    
    ---------
    
    Ref #48 
    Ref #49
    Closes #160
    
    Signed-off-by: AlexNg <contact@ngjx.org>

---
+++ server/src/db/index.ts
@@ -0,0 +1,11 @@
+/**
+ * SPDX-FileCopyrightText: 2024 Ng Jun Xiang <contact@ngjx.org>
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql);
---
+++ server/src/db/schemas.ts
@@ -0,0 +1,17 @@
+/**
+ * SPDX-FileCopyrightText: 2024 Ng Jun Xiang <contact@ngjx.org>
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+
+export const usersTable = pgTable('users_table', {
+  id: serial('id').primaryKey(),
+  name: text('name').notNull(),
+  email: text('email').notNull().unique(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at')
+    .notNull()
+    .$onUpdate(() => new Date()),
+});
