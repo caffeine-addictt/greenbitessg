09caec298bc1663e858c931abdf9a3f14011bd44
AlexNg <contact@ngjx.org>
2024-06-23 00:09:40 +0800

    feat: Add zod error strings to API types
    Signed-off-by: AlexNg <contact@ngjx.org>

--- shared/api-types/src/auth.ts
+++ shared/api-types/src/auth.ts
@@ -13,17 +13,7 @@ export interface AvailabilityAPI
   extends SuccessResponse<{ available: boolean }> {}
 
 /**
- * Successful response for /v1/register endpoint
  */
-export interface RegisterSuccAPI
-  extends SuccessResponse<{ created: boolean }, 201> {}
-export type RegisterFailAPI = ErrorResponse<
-  | 'Username already exists'
-  | 'Email already exists'
-  | 'Email could not be reached'
-  | 'Username is not valid'
-  | 'Email is not valid'
-  | 'Password is not valid'
 >;
 
 /**
@@ -32,8 +22,33 @@ export type RegisterFailAPI = ErrorResponse<
 export interface LoginSuccAPI
   extends SuccessResponse<{ access_token: string; refresh_token: string }> {}
 export type LoginFailAPI = ErrorResponse<
-  | 'Please provide an email'
-  | 'Please provide a password'
-  | 'Email is not valid'
+  | 'Email is not valid!'
+  | 'Please provide an email!'
+  | 'Please provide a password!'
   | 'Invalid email or password'
+>;
+
+/**
+ * Successful response for /v1/register endpoint
+ */
+export interface RegisterSuccAPI
+  extends SuccessResponse<{ created: boolean }, 201> {}
+export type RegisterFailAPI = ErrorResponse<
+  | 'Please provide a username!'
+  | 'Username needs to be at least 3 characters!'
+  | 'Username cannot be longer than 20 characters!'
+  | 'Username may only contain alphanumeric characters and (-_)'
+  | 'Please provide an email!'
+  | 'Email is not valid!'
+  | 'Please provide a password!'
+  | 'Password needs to be at least 8 characters!'
+  | 'Password needs to contain at least 1 lower case character! (a-z)'
+  | 'Password needs to contain at least 1 upper case character! (A-Z)'
+  | 'Password needs to contain at least 1 digit! (0-9)'
+  | 'Password needs to contain at least 1 special character! (!#$%&?\'")'
+  | 'Please retype your password!'
+  | 'Please agree to our Terms of Service!'
+  | 'Email could not be reached'
+  | 'Username already exists'
+  | 'Email already exists'
 >;
