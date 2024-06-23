acb337dd1dc01e6ad899cd43ea811d6f0ad262bb
Jy <tanjy0206@gmail.com>
2024-06-24 02:04:10 +0800

    feat: setting up custom color theme

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -43,7 +43,7 @@ export const Layout = (): JSX.Element => {
   const location = useLocation();
 
   return (
-    <main className="flex min-h-screen min-w-full max-w-full flex-col">
+    <main className="flex min-h-screen min-w-full max-w-full flex-col bg-background-dark text-text-dark">
       <Navbar location={location} isAdmin />
 
       <QueryClientProvider client={new QueryClient()}>
