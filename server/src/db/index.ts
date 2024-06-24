97a9dfd880d959283c74b7e1c53e5a405be72998
AlexNg <contact@ngjx.org>
2024-06-24 21:18:13 +0800

    feat: Add schemas to db
    Signed-off-by: AlexNg <contact@ngjx.org>

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -7,5 +7,7 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 
+import * as schemas from './schemas';
+
 const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle(sql);
+export const db = drizzle(sql, { schema: schemas });
