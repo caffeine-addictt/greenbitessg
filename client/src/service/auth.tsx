69d15d8ad753931367cd12bce41a8fd6f0c60e89
AlexNg <contact@ngjx.org>
2024-06-24 12:21:32 +0800

    feat: Add user to auth context
    Signed-off-by: AlexNg <contact@ngjx.org>

--- client/src/service/auth.tsx
+++ client/src/service/auth.tsx
@@ -4,13 +4,22 @@
  * SPDX-License-Identifier: GPL-3.0-only
  */
 
-import { auth } from '@lib/api-types';
-import { setAuthCookie, unsetAuthCookie, getAuthCookie } from '@utils/jwt';
+import * as z from 'zod';
+import { AxiosError, isAxiosError } from 'axios';
 import { createContext, useState, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
+import httpClient from '@utils/http';
+import { auth } from '@lib/api-types';
+import { userType } from '@lib/api-types/schemas/user';
+import { GetUserSuccAPI } from '@lib/api-types/user';
+import { RefreshFailAPI, RefreshSuccAPI } from '@lib/api-types/auth';
+import { getAuthCookie, setAuthCookie, unsetAuthCookie } from '@utils/jwt';
+import { refreshTokenSchema } from '@lib/api-types/schemas/auth';
+
 export type AuthContextType = {
   state: AuthProviderState;
+  user: z.infer<typeof userType> | null;
   isLoggedIn: boolean;
   isAdmin: boolean;
   login: (tokens: auth.LoginSuccAPI['data']) => void;
@@ -18,12 +27,17 @@ export type AuthContextType = {
 };
 export const AuthContext = createContext<AuthContextType | null>(null);
 
+/** Try to get user info */
+const getUserInfo = () =>
+  httpClient.get<GetUserSuccAPI>({ uri: '/user', withCredentials: 'access' });
+
 export type AuthProviderState = 'pending' | 'done';
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const navigate = useNavigate();
   const location = useLocation();
 
   const [state, setState] = useState<AuthProviderState>('pending');
+  const [user, setUser] = useState<AuthContextType['user']>(null);
   const [isAdmin, setIsAdmin] = useState<boolean>(false);
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
 
@@ -50,6 +64,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
       children={children}
       value={{
         state: state,
+        user: user,
         isLoggedIn: isLoggedIn,
         isAdmin: isAdmin,
         logout: () => {
