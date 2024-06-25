95add97dd9c6ab1de6a44577f7b55599fbcdad44
Jy <tanjy0206@gmail.com>
2024-06-25 11:51:37 +0800

    fix: extending color theme

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -9,30 +9,31 @@ import type { Config } from 'tailwindcss';
 export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      background: {
-        light: '#F1FEF4',
-        dark: '#2A2928',
-      },
-      primary: {
-        light: '#3CC360',
-        dark: '#CDBCB1',
-      },
-      secondary: {
-        light: '#7EAAF7',
-        dark: '#5ACA41',
-      },
-      accent: {
-        light: '#5552F4',
-        dark: '#73A586',
-      },
-      text: {
-        light: '#010E04',
-        dark: '#EBE5E0',
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        background: {
+          light: '#F1FEF4',
+          dark: '#2A2928',
+        },
+        primary: {
+          light: '#3CC360',
+          dark: '#CDBCB1',
+        },
+        secondary: {
+          light: '#7EAAF7',
+          dark: '#5ACA41',
+        },
+        accent: {
+          light: '#5552F4',
+          dark: '#73A586',
+        },
+        text: {
+          light: '#010E04',
+          dark: '#EBE5E0',
+        },
       },
     },
-    extend: {},
   },
   plugins: [],
 } satisfies Config;
