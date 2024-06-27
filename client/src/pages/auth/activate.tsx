d14719dc77a6ef50cc103a0a1d592f02cc642e40
AlexNg <contact@ngjx.org>
2024-06-27 08:21:43 +0800

    feat: /activate/:token page styling
    Signed-off-by: AlexNg <contact@ngjx.org>

--- client/src/pages/auth/activate.tsx
+++ client/src/pages/auth/activate.tsx
@@ -15,6 +15,13 @@ import { AuthContext } from '@service/auth';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { activateFormSchema } from '@lib/api-types/schemas/auth';
 
+import { cn } from '@utils/tailwind';
+import {
+  SymbolIcon,
+  CheckCircledIcon,
+  CrossCircledIcon,
+} from '@radix-ui/react-icons';
+
 // Page
 const ActivateWithTokenPage: PageComponent = ({
   className,
@@ -26,7 +33,7 @@ const ActivateWithTokenPage: PageComponent = ({
   const queryClient = useQueryClient();
 
   // Handle activate
-  const { refetch, isFetching, isSuccess, isError } = useQuery(
+  const { isFetching, isSuccess, isError } = useQuery(
     {
       queryKey: ['activate'],
       queryFn: () =>
@@ -53,11 +60,41 @@ const ActivateWithTokenPage: PageComponent = ({
   }
 
   return (
-    <div className={className} {...props}>
-      {isFetching && 'Activating your account. Please wait.'}
-      {isSuccess && 'Your account has been activated. Please login.'}
-      {isError && 'Failed to activate your account. Please try again later.'}
-      <button onClick={() => refetch()}>refetch</button>
+    <div
+      className={cn(className, 'flex-col pt-32 mx-auto text-center')}
+      {...props}
+    >
+      {/* Header */}
+      <h1 className="mb-4 text-3xl font-bold">Activate your account</h1>
+
+      {/* Sub header */}
+      <h3 className="mb-32 text-lg">
+        Please check your email for the activation link.
+      </h3>
+
+      {/* Statuses */}
+      <div className="transition-all">
+        {isFetching && (
+          <div className="flex flex-row items-center justify-center gap-2">
+            <SymbolIcon className="mr-2 size-6 animate-spin" />
+            Activating your account... Please wait.
+          </div>
+        )}
+
+        {isSuccess && (
+          <div className="flex flex-row items-center justify-center gap-2">
+            <CheckCircledIcon className="mr-2 size-6 animate-none text-green-500" />
+            Your account has been activated. Please login.
+          </div>
+        )}
+
+        {isError && (
+          <div className="flex flex-row items-center justify-center gap-2">
+            <CrossCircledIcon className="mr-2 size-6 text-red-500" />
+            Failed to activate your account. Please try again later.
+          </div>
+        )}
+      </div>
     </div>
   );
 };
