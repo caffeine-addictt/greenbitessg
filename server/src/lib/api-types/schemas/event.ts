3dbd538d71e6b861e24432d4854de26076cdcb64
Jy <tanjy0206@gmail.com>
2024-06-28 10:52:02 +0800

    chore(sync): Sync shared/api-types
    Co-authored-by: AlexNg <contact@ngjx.org>

--- server/src/lib/api-types/schemas/event.ts
+++ server/src/lib/api-types/schemas/event.ts
@@ -12,3 +12,31 @@ export const eventType = z.object({
   location: z.string().min(1),
   description: z.string().min(1),
 });
+
+export const createEventFormSchema = z.object({
+  namename: z
+    .string({
+      invalid_type_error: 'Please provide an event name!',
+      required_error: 'Please provide an event name!',
+    })
+    .min(1, { message: ' Please provide an event name!' })
+    .min(3, { message: 'Event name needs to be at least 3 characters!' })
+    .max(20, { message: 'Event name cannot be longer than 20 characters!' })
+    .regex(/^[\w\d-_]+$/, {
+      message: 'Event name may only contain alphanumeric characters and (-_)',
+    }),
+  location: z
+    .string({
+      invalid_type_error: 'Please provide a location',
+      required_error: 'Please provide a location',
+    })
+    .min(1, { message: 'Please provide a location' }),
+
+  description: z
+    .string({
+      invalid_type_error: 'Please provide a description!',
+      required_error: 'Please provide a description!',
+    })
+    .min(1, { message: 'Please provide a description!' })
+    .min(8, { message: 'description needs to be at least 8 characters!' }),
+});
