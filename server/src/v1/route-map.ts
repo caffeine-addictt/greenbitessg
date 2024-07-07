3094dc02d7a99d3cec2228cfd428bf01177b4fe1
AlexNg <contact@ngjx.org>
2024-07-08 01:43:41 +0800

    feat: Make routingMap type URL more flexible
    Signed-off-by: AlexNg <contact@ngjx.org>

--- server/src/v1/route-map.ts
+++ server/src/v1/route-map.ts
@@ -4,13 +4,7 @@
  * SPDX-License-Identifier: GPL-3.0-only
  */
 
-import type {
-  RoutingMap as IndexRoutingMap,
-  RouteHandlers,
-} from '../route-map';
-interface RoutingMap extends IndexRoutingMap {
-  [path: `/v1/${string}`]: RouteHandlers;
-}
+import type { RoutingMap } from '../route-map';
 
 // Import endpoints
 import {
@@ -24,7 +18,7 @@ import {
 import { availability } from './availability';
 import { getUser } from './user';
 
-const routeMap: RoutingMap = {
+const routeMap: RoutingMap<`/v1/${string}`> = {
   '/v1/auth/login': {
     POST: { handler: login },
   },
