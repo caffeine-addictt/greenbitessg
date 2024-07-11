99b27a6f7f4aae27585aabfb4340c9abbceb24f6
AlexNg <contact@ngjx.org>
2024-07-11 22:32:35 +0800

    chore: Add jest config
    Signed-off-by: AlexNg <contact@ngjx.org>

---
+++ server/jest.config.js
@@ -0,0 +1,13 @@
+/**
+ * SPDX-FileCopyrightText: 2024 Ng Jun Xiang <contact@ngjx.org>
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+const config = {
+  verbose: true,
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+};
+export default config;
