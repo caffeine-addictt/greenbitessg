0d395b9b028adfb9e06f2db00304f74642058841
ArshiLamba <Arshilamba04@gmail.com>
2024-07-21 21:58:53 +0800

    feat(client): create route map to di splay the page in the application

---
+++ client/src/pages/user/route-map.tsx
@@ -0,0 +1,19 @@
+/**
+ * SPDX-FileCopyrightText: 2024 Ng Jun Xiang <contact@ngjx.org>
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+import type { PageComponent, RouteMap } from '@pages/route-map';
+
+// Import pages
+import FeedbackForm from './feedback';
+
+const routeMap: RouteMap = {
+  '/feedback': {
+    title: 'Feedback Form',
+    description: 'State feedback here',
+    component: FeedbackForm as PageComponent,
+  },
+} as const;
+export default routeMap;
