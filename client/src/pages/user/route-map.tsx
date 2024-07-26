af8434ec265d49cb66d64b05ed80358763266025
ArshiLamba <Arshilamba04@gmail.com>
2024-07-26 19:31:03 +0800

    feat: created route-map.tsx and added employee route

---
+++ client/src/pages/user/route-map.tsx
@@ -0,0 +1,21 @@
+/**
+ * SPDX-FileCopyrightText: 2024 Ng Jun Xiang <contact@ngjx.org>
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+import type { PageComponent, RouteMap } from '@pages/route-map';
+
+// Import pages
+import EmployeeMain from './employee';
+
+const routeMap1: RouteMap = {
+  '/employee': {
+    title: 'Employee Details',
+    description: 'Fill in your details here',
+    component: EmployeeMain as PageComponent,
+    accessLevel: 'authenticated',
+  },
+  
+} as const;
+export default routeMap1;
