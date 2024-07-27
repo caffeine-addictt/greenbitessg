342084789d767ecc498973042f3d85ba6a30d6b0
ArshiLamba <Arshilamba04@gmail.com>
2024-07-27 17:15:30 +0800

    feat: add API routes for dashboard

--- server/src/v1/route-map.ts
+++ server/src/v1/route-map.ts
@@ -1,14 +1,9 @@
-/**
- * SPDX-FileCopyrightText: 2024 Ng Jun Xiang <contact@ngjx.org>
- *
- * SPDX-License-Identifier: GPL-3.0-only
- */
-
 import type { RoutingMap } from '../route-map';
 
 // Import endpoints
 import authRoutes from './auth/route-map';
 import { availability } from './availability';
+import { getDashboard } from './dashboard';
 import { getUser } from './user';
 
 const routeMap: RoutingMap<`/v1/${string}`> = {
@@ -21,6 +16,12 @@ const routeMap: RoutingMap<`/v1/${string}`> = {
       handler: getUser,
       accessLevel: 'authenticated',
       authOptions: { allowNonActivated: true },
+    },
+  },
+  '/v1/dashboard': {
+    GET: {
+      handler: getDashboard,
+      accessLevel: 'authenticated',
     },
   },
 } as const;
