582320e7254fb639eef8b9a5212ecce08d603d2e
AlexNg <contact@ngjx.org>
2024-07-29 00:39:39 +0800

    feat: Add new required API keys
    Signed-off-by: AlexNg <contact@ngjx.org>

--- server/src/check-env.ts
+++ server/src/check-env.ts
@@ -22,3 +22,6 @@ requireEnv('JWT_REFRESH_KEY');
 requireEnv('RESEND_API_KEY');
 requireEnv('UPLOADTHING_APP_ID');
 requireEnv('UPLOADTHING_SECRET');
+requireEnv('RECOGNITION_API_TOKEN');
+requireEnv('NUTRITION_APP_KEY');
+requireEnv('NUTRITION_APP_ID');
