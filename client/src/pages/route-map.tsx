590c55c510a0bba4af3815092ef857688c403dbb
ArshiLamba <Arshilamba04@gmail.com>
2024-07-28 12:31:08 +0800

    refactor: rectify naming conventions for route maps in route-map.tsx

--- client/src/pages/route-map.tsx
+++ client/src/pages/route-map.tsx
@@ -9,8 +9,8 @@ import * as React from 'react';
 // Page imports
 import NotFound from '@pages/404';
 import RootPage from '@pages/root';
-import routeMap from '@pages/auth/route-map';
-import routeMap1 from './user/route-map';
+import authrouteMap from '@pages/auth/route-map';
+import userrouteMap from './user/route-map';
 
 export type PathStr = `/${string}`;
 export type RootPathStr = '*' | '/' | PathStr;
@@ -40,7 +40,7 @@ const routes: RootRouteMap = {
     accessLevel: 'admin',
     title: 'Admin',
   },
-  ...routeMap,
-  ...routeMap1,
+  ...authrouteMap,
+  ...userrouteMap,
 } as const;
 export default routes;
