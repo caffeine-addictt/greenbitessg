c0c879aacff9bc1b3158df0a40c65cfbc7d00370
AlexNg <contact@ngjx.org>
2024-07-29 20:45:15 +0800

    feat: Add /v1/food DELTE endpoint
    Signed-off-by: AlexNg <contact@ngjx.org>

--- server/src/v1/food/route-map.ts
+++ server/src/v1/food/route-map.ts
@@ -5,6 +5,7 @@
  */
 
 import type { RoutingMap } from '../../route-map';
+import deleteFood from './delete';
 import getFood from './food';
 import { postGenerate } from './generate';
 
@@ -15,6 +16,7 @@ const routeMap: RoutingMap<`/v1/food${string}`> = {
   },
   '/v1/food/:id': {
     GET: { handler: getFood },
+    DELETE: { handler: deleteFood },
     accessLevel: 'authenticated',
   },
   '/v1/food': {
