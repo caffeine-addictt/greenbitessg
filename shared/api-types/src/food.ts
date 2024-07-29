aa5f00c9b542a4cefa5cf75ac060a8fb7ec8d35d
Jun Xiang (カエデ) <contact@ngjx.org>
2024-07-29 20:29:21 +0800

    feat: Add /v1/food and /v1/food/:id GET api (#239)
    Signed-off-by: AlexNg <contact@ngjx.org>

--- shared/api-types/src/food.ts
+++ shared/api-types/src/food.ts
@@ -7,6 +7,29 @@
 import type { SuccessResponse, ErrorResponse } from './index';
 
 /**
+ * Successful response for /v1/food endpoint
+ */
+type Food = {
+  id: number;
+  name: string;
+  servingUnit: string;
+  servingQty: number;
+  calories: number;
+  imageUrl: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+export interface FoodGetSuccessAPI extends SuccessResponse<Food[]> {}
+export interface FoodGetIdSuccessAPI extends SuccessResponse<Food> {}
+export type FoodGetFailAPI = ErrorResponse<
+  | 'Invalid ID!'
+  | 'ID does not exist!'
+  | 'Invalid query!'
+  | 'Invalid limit!'
+  | 'Invalid page!'
+>;
+
+/**
  * Successful response for /v1/food/generate endpoint
  */
 export interface FoodGenerateSuccessAPI
