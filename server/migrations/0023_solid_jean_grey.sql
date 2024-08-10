e792519c871c13e4392f5effc4108021cd92eaa8
AlexNg <contact@ngjx.org>
2024-08-10 22:01:14 +0800

    fix: Generated DB migration failing
    Signed-off-by: AlexNg <contact@ngjx.org>

--- server/migrations/0023_solid_jean_grey.sql
+++ server/migrations/0023_solid_jean_grey.sql
@@ -1,2 +1,4 @@
-ALTER TABLE "passkeys_table" ALTER COLUMN "id" SET DATA TYPE serial;--> statement-breakpoint
-ALTER TABLE "passkeys_table" ADD COLUMN "credential_id" text NOT NULL;
+ALTER TABLE "passkeys_table" RENAME  COLUMN "id" TO "credential_id";--> statement-breakpoint
+ALTER TABLE "passkeys_table" ALTER COLUMN "credential_id" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "passkeys_table" DROP CONSTRAINT "passkeys_table_pkey";--> statement-breakpoint
+ALTER TABLE "passkeys_table" ADD COLUMN "id" serial NOT NULL PRIMARY KEY;
