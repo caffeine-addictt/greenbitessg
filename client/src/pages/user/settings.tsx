af000cec50c326ef9beb6156caef1a747303b551
AlexNg <contact@ngjx.org>
2024-08-10 17:46:50 +0800

    feat: Abstract danger and details
    Signed-off-by: AlexNg <contact@ngjx.org>

--- client/src/pages/user/settings.tsx
+++ client/src/pages/user/settings.tsx
@@ -33,11 +33,73 @@ import { PageComponent } from '@pages/route-map';
 import { useNavigate } from 'react-router-dom';
 import { cn } from '@utils/tailwind';
 
-const AccountSettings: PageComponent = ({ className, ...props }) => {
-  const { user, refetch } = useContext(AuthContext)!;
-  const queryClient = useQueryClient();
-  const { toast } = useToast();
+const AccountSettingsPage: PageComponent = ({ className, ...props }) => (
+  <div
+    {...props}
+    className={cn(
+      className,
+      'flex flex-col mx-auto py-10 w-[90%] sm:w-4/5 md:w-2/3 lg:w-1/3',
+    )}
+  >
+    <h1 className="mb-10 text-center text-5xl font-bold">My Account</h1>
+    <AccountDetails />
+    <AccountDangerActions />
+  </div>
+);
+
+export default AccountSettingsPage;
+
+// Danger Actions
+export const AccountDangerActions = () => {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  const { toast } = useToast();
+
+  // Handle deleting account
+  const { mutate: deleteUser } = useMutation(
+    {
+      mutationKey: ['delete-user'],
+      mutationFn: () => {
+        return httpClient.delete<SuccessResponse<null>>({
+          uri: '/user',
+          withCredentials: 'access',
+        });
+      },
+      onError: (err) => {
+        console.log(err);
+        toast({
+          title: 'Something went wrong',
+          description: isAxiosError(err)
+            ? err.response?.data.errors[0].message
+            : 'Please try again later',
+          variant: 'destructive',
+        });
+      },
+      onSuccess: () => {
+        navigate('/');
+      },
+    },
+    queryClient,
+  );
+
+  return (
+    <div className="my-7 flex w-full flex-col justify-center gap-5 rounded-md border-2 border-red-400 bg-primary-dark p-4">
+      <h2 className="text-3xl font-bold text-red-900">Danger Zone</h2>
+
+      <Button type="button" variant="destructive" onClick={() => deleteUser()}>
+        Delete My Account
+      </Button>
+    </div>
+  );
+};
+
+// Account Details
+export const AccountDetails = () => {
+  const queryClient = useQueryClient();
+
+  const { toast } = useToast();
+  const { user, refetch } = useContext(AuthContext)!;
 
   const accountSettingsForm = useForm<z.infer<typeof userUpdateSchema>>({
     resolver: zodResolver(userUpdateSchema),
@@ -82,138 +144,87 @@ const AccountSettings: PageComponent = ({ className, ...props }) => {
     queryClient,
   );
 
-  // Handle deleting account
-  const { mutate: deleteUser } = useMutation(
-    {
-      mutationKey: ['delete-user'],
-      mutationFn: () => {
-        return httpClient.delete<SuccessResponse<null>>({
-          uri: '/user',
-          withCredentials: 'access',
-        });
-      },
-      onError: (err) => {
-        console.log(err);
-        toast({
-          title: 'Something went wrong',
-          description: isAxiosError(err)
-            ? err.response?.data.errors[0].message
-            : 'Please try again later',
-          variant: 'destructive',
-        });
-      },
-      onSuccess: () => {
-        navigate('/');
-      },
-    },
-    queryClient,
-  );
-
   return (
-    <div
-      {...props}
-      className={cn(
-        className,
-        'flex flex-col mx-auto py-10 w-[90%] sm:w-4/5 md:w-2/3 lg:w-1/3',
-      )}
-    >
-      <h1 className="mb-10 text-center text-5xl font-bold">My Account</h1>
-      <div className="mx-auto my-7 block w-full rounded-md bg-primary-dark p-10 text-lg dark:text-text-light">
-        <h4 className="mb-3 flex justify-center font-semibold underline">
-          Personal Particulars
-        </h4>
-        <div className="mx-auto flex justify-center">
-          <Form {...accountSettingsForm}>
-            <form
-              onSubmit={accountSettingsForm.handleSubmit((data) =>
-                updateUser(data),
+    <div className="mx-auto my-7 block w-full rounded-md bg-primary-dark p-10 text-lg dark:text-text-light">
+      <h4 className="mb-3 flex justify-center font-semibold underline">
+        Personal Particulars
+      </h4>
+      <div className="mx-auto flex justify-center">
+        <Form {...accountSettingsForm}>
+          <form
+            onSubmit={accountSettingsForm.handleSubmit((data) =>
+              updateUser(data),
+            )}
+            className="mt-8 w-[26.5rem] space-y-4"
+          >
+            <FormField
+              control={accountSettingsForm.control}
+              name="username"
+              render={({ field, fieldState }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Username"
+                      className={fieldState.error ? 'border-red-700' : ''}
+                      {...field}
+                    />
+                  </FormControl>
+                  {fieldState.error ? (
+                    <FormMessage>{fieldState.error.message}</FormMessage>
+                  ) : (
+                    <FormDescription>Your account username.</FormDescription>
+                  )}
+                </FormItem>
               )}
-              className="mt-8 w-[26.5rem] space-y-4"
-            >
-              <FormField
-                control={accountSettingsForm.control}
-                name="username"
-                render={({ field, fieldState }) => (
-                  <FormItem>
-                    <FormLabel>Username</FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="Username"
-                        className={fieldState.error ? 'border-red-700' : ''}
-                        {...field}
-                      />
-                    </FormControl>
-                    {fieldState.error ? (
-                      <FormMessage>{fieldState.error.message}</FormMessage>
-                    ) : (
-                      <FormDescription>Your account username.</FormDescription>
-                    )}
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={accountSettingsForm.control}
-                name="email"
-                render={({ field, fieldState }) => (
-                  <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="johndoe@example.com"
-                        className={fieldState.error ? 'border-red-700' : ''}
-                        {...field}
-                      />
-                    </FormControl>
-                    {fieldState.error ? (
-                      <FormMessage>{fieldState.error.message}</FormMessage>
-                    ) : (
-                      <FormDescription>Your email address.</FormDescription>
-                    )}
-                  </FormItem>
-                )}
-              />
-              <div className="flex justify-center space-x-2 md:justify-end">
-                <Button
-                  type="reset"
-                  variant="ghost"
-                  disabled={
-                    isSubmitting || !accountSettingsForm.formState.isDirty
-                  }
-                >
-                  Cancel
-                </Button>
-                <Button
-                  type="submit"
-                  variant="secondary"
-                  disabled={
-                    isSubmitting ||
-                    (user?.username ===
-                      accountSettingsForm.getValues('username') &&
-                      user?.email === accountSettingsForm.getValues('email'))
-                  }
-                >
-                  {isSubmitting ? 'Saving...' : 'Save'}
-                </Button>
-              </div>
-            </form>
-          </Form>
-        </div>
-      </div>
-      <div className="my-7 flex w-full justify-center rounded-md bg-primary-dark p-4">
-      {/* Danger zone */}
-      <div className="bg-primary-dark my-7 flex w-full flex-col justify-center gap-5 rounded-md border-2 border-red-400 p-4">
-        <h2 className="text-3xl font-bold text-red-900">Danger Zone</h2>
-
-        <Button
-          type="button"
-          variant="destructive"
-          onClick={() => deleteUser()}
-        >
-          Delete My Account
-        </Button>
+            />
+            <FormField
+              control={accountSettingsForm.control}
+              name="email"
+              render={({ field, fieldState }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="johndoe@example.com"
+                      className={fieldState.error ? 'border-red-700' : ''}
+                      {...field}
+                    />
+                  </FormControl>
+                  {fieldState.error ? (
+                    <FormMessage>{fieldState.error.message}</FormMessage>
+                  ) : (
+                    <FormDescription>Your email address.</FormDescription>
+                  )}
+                </FormItem>
+              )}
+            />
+            <div className="flex justify-center space-x-2 md:justify-end">
+              <Button
+                type="reset"
+                variant="ghost"
+                disabled={
+                  isSubmitting || !accountSettingsForm.formState.isDirty
+                }
+              >
+                Cancel
+              </Button>
+              <Button
+                type="submit"
+                variant="secondary"
+                disabled={
+                  isSubmitting ||
+                  (user?.username ===
+                    accountSettingsForm.getValues('username') &&
+                    user?.email === accountSettingsForm.getValues('email'))
+                }
+              >
+                {isSubmitting ? 'Saving...' : 'Save'}
+              </Button>
+            </div>
+          </form>
+        </Form>
       </div>
     </div>
   );
 };
-
-export default AccountSettings;
