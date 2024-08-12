9eb8cc0d61aa49f6db8098ae96250a115068daef
KenNoYu <tanjy0206@gmail.com>
2024-08-13 04:28:00 +0800

    feat: event list only for viewing

--- client/src/pages/user/event-list.tsx
+++ client/src/pages/user/event-list.tsx
@@ -16,7 +16,7 @@ import { useNavigate } from 'react-router-dom';
 // Define the Event type using z.infer and eventSchema
 type Event = z.infer<typeof eventSchema>;
 
-const EventManage: PageComponent = () => {
+const EventList: PageComponent = () => {
   const [events, setEvents] = useState<Event[]>([]);
   const [error, setError] = useState<string | null>(null);
   const navigate = useNavigate();
@@ -45,23 +45,26 @@ const EventManage: PageComponent = () => {
 
   return (
     <div className="container mx-auto mt-16">
-      <h1 className="text-center text-2xl font-bold mb-4">Upcoming Events</h1>
+      <h1 className="mb-4 text-center text-2xl font-bold">Upcoming Events</h1>
       {error && <p className="text-red-500">{error}</p>}
       {events.length > 0 ? (
-        <ul className='grid'>
+        <ul className="grid">
           {events.map((event) => (
             <li
               key={event.id}
-              className="grid grid-cols-1 md:grid-cols-2 bg-primary-dark mb-6 rounded-md border text-text-light border-gray-300"
+              className="mb-6 grid grid-cols-1 rounded-md border border-gray-300 bg-primary-dark text-text-light md:grid-cols-2"
             >
-              <div className='p-10'>
+              <div className="p-10">
                 <h2 className="text-xl font-semibold">{event.title}</h2>
                 <p>{`${event.id}`}</p>
                 <p>Date: {`${event.date}`}</p>
                 <p>Location: {event.location}</p>
               </div>
-              <div className='relative mb-2 mr-2'>
-                <Button className='p-4 !bg-primary-light !text-text-dark text-base font-semibold md:absolute md:bottom-0 md:right-0' onClick={() => redirectToEvent(event.id)}>
+              <div className="relative mb-2 mr-2">
+                <Button
+                  className="!bg-primary-light p-4 text-base font-semibold !text-text-dark md:absolute md:bottom-0 md:right-0"
+                  onClick={() => redirectToEvent(event.id)}
+                >
                   Learn More
                 </Button>
               </div>
@@ -75,4 +78,4 @@ const EventManage: PageComponent = () => {
   );
 };
 
-export default EventManage;
+export default EventList;
