c09eda5ee7c12e24d577c33140b0d5c4e4c0e19f
Jun Xiang (カエデ) <contact@ngjx.org>
2024-08-12 22:04:59 +0800

    feat: Add privacy page (#279)
    Signed-off-by: AlexNg <contact@ngjx.org>

--- client/src/pages/legal/route-map.tsx
+++ client/src/pages/legal/route-map.tsx
@@ -6,12 +6,18 @@
 
 import type { RouteMap } from '@pages/route-map';
 import TosPage from './tos';
+import PrivacyPage from './privacy';
 
 const legalRouteMap: RouteMap = {
   '/tos': {
     title: 'Terms of Service',
     description: 'Our Terms of Service',
     component: TosPage,
+  },
+  '/privacy': {
+    title: 'Privacy Policy',
+    description: 'Our Privacy Policy',
+    component: PrivacyPage,
   },
 };
 export default legalRouteMap;
