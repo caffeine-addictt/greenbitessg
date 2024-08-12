0d2b71329290f9a80a21dc00718d2045f5794f05
KenNoYu <tanjy0206@gmail.com>
2024-08-13 06:52:35 +0800

    feat: homepage image with images and LCP

--- client/src/pages/root.tsx
+++ client/src/pages/root.tsx
@@ -9,22 +9,24 @@ import { Button } from '@components/ui/button';
 
 const RootPage: PageComponent = (props) => {
   return (
-    <div {...props} className="flex-col items-center">
+    <div {...props} className="flex-col items-center mb-10">
       <div className="relative text-text-light">
-        <img src='../public/lcp-screen.png' className='min-w-full h-auto object-fill' loading='eager'></img>
-        <div className="absolute bottom-3 left-5 md:bottom-1/3 lg:bottom-1/2 md:left-10 mb-4">
-          <h1 className='text-sm md:text-2xl lg:text-4xl font-bold mb-4'>Driving sustainable food possibilties</h1>
-          <Button variant="secondary" size="default"  className='w-1/2 md:w-full md:w-auto'>
-            Start Now
-          </Button>
+        <img src='/lcp-screen-1366x600.webp' className='h-auto min-w-full object-fill' loading='eager'></img>
+        <div className="absolute bottom-6 left-1 mb-4 md:bottom-1/3 md:left-10 lg:bottom-1/2">
+          <h1 className='mb-4 text-base font-bold md:text-2xl lg:text-4xl'>Driving sustainable food possibilties</h1>
+          <a href="/register" className="flex justify-normal">
+            <Button variant="secondary" size="default"  className='w-20 md:w-auto !bg-primary-light'>
+              Start Now
+            </Button>
+          </a>
         </div>
       </div>
 
       <div className="container mx-auto mt-16 flex-col">
         <h1 className="text-center text-2xl font-bold ">About Us</h1>
-        <div className="mt-8 flex flex-col items-center md:flex-row">
-          <img className="w-full md:w-1/2" alt=""></img>
-          <div className="mt-4 w-full md:ml-0 md:mt-0 md:w-1/2">
+        <div className="mt-8 flex flex-col items-center md:flex-row gap-10">
+          <img src='/sustainability-image2-1445x813.webp' className="w-full md:w-1/2 md:h-auto" loading='lazy'></img>
+          <div className="mt-4 w-full md:ml-0 md:mt-0 md:w-1/2 ">
             <h3 className="text-center text-2xl"> What is GreenBitesSG?</h3>
             <p className="mt-3">
               Here at GreenBitesSG we are a nation wide movement that focuses on
@@ -34,11 +36,10 @@ const RootPage: PageComponent = (props) => {
             </p>
           </div>
         </div>
-        <div className="mt-8 flex flex-col items-center md:flex-row-reverse">
-          <img className="w-full md:w-1/2" alt=""></img>
+        <div className="mt-8 flex flex-col items-center md:flex-row-reverse gap-10">
+          <img src='/sustainability-image3-1600x900.webp' className="w-full md:w-1/2" loading='lazy'></img>
           <div className="mt-4 w-full md:ml-0 md:mt-0 md:w-1/2">
             <h3 className="text-center text-2xl">
-              {' '}
               Why is GreenBitesSG important?
             </h3>
             <p className="mt-7">
@@ -51,24 +52,24 @@ const RootPage: PageComponent = (props) => {
       </div>
 
       <div className="container mx-auto mt-20">
-        <h1 className="text-center text-2xl font-bold">Our Partners</h1>
-        <div className="flex flex-col items-center md:flex-row">
-          <img className="w-full md:w-1/2" alt=""></img>
-          <img className="w-full md:w-1/2" alt=""></img>
+        <h1 className="text-center text-2xl font-bold mb-8">Our Partners</h1>
+        <div className="flex flex-col items-center md:flex-row justify-center space-y-4 md:gap-32 mb-3">
+          <img src='/SFA-logo-200x93.webp' className="mb-10 md:m-0" alt="" loading='lazy'></img>
+          <img src='/NEA-logo-400x181.webp' className="mt-10 md:m-0 w-60 h:-auto" alt="" loading='lazy'></img>
         </div>
       </div>
 
       <div className="container mx-auto mt-20">
         <h1 className="text-center text-2xl font-bold">Start Now</h1>
-        <div className="mt-8 flex flex-col items-center md:flex-row">
-          <img className="w-full md:w-1/2" alt=""></img>
+        <div className="mt-8 flex flex-col items-center md:flex-row gap-10">
+          <img src='/sustainability-1000x562.webp' className="w-full md:w-1/2" alt="" loading='lazy'></img>
           <div className="mt-4 w-full md:ml-0 md:mt-0 md:w-1/2">
             <p className="mb-4">
               So what are you waiting for? Food wastage won't wait for you so
               join us and the community in combatting food wastage!
             </p>
             <a href="/register" className="flex justify-center">
-              <Button variant="secondary" size="default">
+              <Button variant="secondary" size="default" className='!bg-primary-light'>
                 Start Now
               </Button>
             </a>
