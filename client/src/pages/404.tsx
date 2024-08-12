8aa58884a96e8c4579d36ed65ffed312b885e4d3
Jun Xiang (カエデ) <contact@ngjx.org>
2024-08-12 12:11:52 +0800

    feat: Improve 404 page styling (#267)
    Signed-off-by: AlexNg <contact@ngjx.org>

--- client/src/pages/404.tsx
+++ client/src/pages/404.tsx
@@ -4,12 +4,22 @@
  * SPDX-License-Identifier: GPL-3.0-only
  */
 
+import { InternalLink } from '@components/ui/button';
 import type { PageComponent } from '@pages/route-map';
+import { cn } from '@utils/tailwind';
 
-const NotFound: PageComponent = (props) => {
+const NotFound: PageComponent = ({ className, ...props }) => {
   return (
-    <div {...props}>
-      <h1>Page Not Found</h1>
+    <div
+      {...props}
+      className={cn(className, 'flex-col gap-2 items-center justify-center')}
+    >
+      <h1 className="text-4xl font-bold">Page Not Found</h1>
+      <p className="text-lg">Sorry, the page you requested was not found.</p>
+
+      <InternalLink href="/" className="mt-20">
+        Return Home
+      </InternalLink>
     </div>
   );
 };
